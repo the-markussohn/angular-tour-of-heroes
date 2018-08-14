2bc191a773268daaa04039cbcb5bee16cb6f877d
the-markussohn <markus.justinas@gmail.com>
2018-08-14 20:04:12 +0300

    hero search

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -61,6 +61,16 @@ export class HeroService {
     );
   }
 
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
